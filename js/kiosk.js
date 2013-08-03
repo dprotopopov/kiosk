c2729b530220255b4c9517acb52126cd1badd88b
Dmitry Protopopov <dmitry.protopopov@gmail.com>
2013-08-03 16:44:57 +0400

    config

--- js/kiosk.js
+++ js/kiosk.js
@@ -808,7 +808,7 @@ function urldecode (str) {
 // Обработка try-catch требуется для совместимости с IE
 function debugWrite(a,b) {
 	try {
-		console.log(a,b);
+		console.log(a+":"+b);
 	} catch (e) {
 	}
 }
